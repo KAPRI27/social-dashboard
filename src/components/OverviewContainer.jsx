c488badbfe2a87ccc898789ec320c2aa90754fd0
Kapri27 <paumau27@gmail.com>
2025-11-02 10:25:37 -0300

    style: responsive design. listo

--- src/components/OverviewContainer.jsx
+++ src/components/OverviewContainer.jsx
@@ -12,8 +12,7 @@ const convertNumberToK = (number) => {
 
 export const OverviewContainer = () => {
     return (
-        <section className='max-w-[1440px]'>
-            <div className='w-[326px]  flex flex-wrap top-[191px] left-0 right-0 mx-auto '>
+            <div className='max-w-[1440px] min-w-[326px] flex flex-wrap gap-[30px] place-content-center top-[191px] left-0 right-0 mx-auto pb-4'>
                 {
                 data.overview.map(object => 
                     <OverviewCard 
@@ -28,15 +27,14 @@ export const OverviewContainer = () => {
                 )   
             }
             </div>
-        </section>
     )
 }
 
 export const OverviewTodayContainer = () => {
     return (
-        <section className='max-w-[1440px]'>
-            <h2 className='text-2xl font-bold mb-[27px] text-dark-grayish-blue ml-8'>Overview Today</h2>
-            <div className='w-[326px]  top-[191px] left-0 right-0 mx-auto'>
+        <section className='max-w-[1440px] min-w-[326px] place-content-center  '>
+            <h2 className='text-2xl font-bold mb-[27px] text-dark-grayish-blue px-5 mt-[30px]'>Overview Today</h2>
+            <div className=' top-[191px] left-0 right-0  mx-auto flex flex-wrap gap-[30px] place-content-center'>
                 {
                     data['overview-today'].map( object => 
                         <OverviewTodayCard
